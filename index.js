cae8c5e4087dd5cdc9ded857dac95f2d5ed1c1f5
storykim <donghwa.s.kim@gmail.com>
2020-02-10 23:24:38 +0100

    Run linter

--- index.js
+++ index.js
@@ -1,83 +1,87 @@
-'use strict';
+"use strict";
 
-var os = require('os');
-var nodeStatic = require('node-static');
-var http = require('http');
-var socketIO = require('socket.io');
+var os = require("os");
+var nodeStatic = require("node-static");
+var http = require("http");
+var socketIO = require("socket.io");
 
-var fileServer = new(nodeStatic.Server)();
-var app = http.createServer(function(req, res) {
-  fileServer.serve(req, res);
-}).listen(8080);
+var fileServer = new nodeStatic.Server();
+var app = http
+  .createServer(function(req, res) {
+    fileServer.serve(req, res);
+  })
+  .listen(8080);
 
 var nextId = 0;
 function generateId() {
   nextId += 1;
   return nextId;
-};
+}
 
 var idToConnMap = {};
 const ROOM_CAPACITY = 4;
 
 var io = socketIO.listen(app);
-io.sockets.on('connection', function(socket) {
+io.sockets.on("connection", function(socket) {
   socket.userId = generateId();
   idToConnMap[socket.userId] = socket;
 
   // convenience function to log server messages on the client
   function log() {
-    var array = ['[ServerLog]'];
+    var array = ["[ServerLog]"];
     array.push.apply(array, arguments);
-    socket.emit('log', array);
+    socket.emit("log", array);
   }
 
-  socket.on('message', function(packedMessage) {
-    var[target, message] = packedMessage;
-    log('Client said: ', target, message);
+  socket.on("message", function(packedMessage) {
+    var [target, message] = packedMessage;
+    log("Client said: ", target, message);
     if (target in idToConnMap) {
-      idToConnMap[target].emit('message', [socket.userId, message]);
+      idToConnMap[target].emit("message", [socket.userId, message]);
     }
   });
-  
-  socket.on('broadcast', function(message) {
+
+  socket.on("broadcast", function(message) {
     // TODO: for a real app, would be room-only (not broadcast)
-    socket.broadcast.emit('message', [socket.userId, message]);
+    socket.broadcast.emit("message", [socket.userId, message]);
   });
 
-  socket.on('disconnect', function(message) {
+  socket.on("disconnect", function(message) {
     delete idToConnMap[socket.userId];
-    socket.broadcast.emit('message', [socket.userId, 'bye']);
+    socket.broadcast.emit("message", [socket.userId, "bye"]);
   });
 
-
-  socket.on('create or join', function(room) {
-    log('Received request to create or join room ' + room);
+  socket.on("create or join", function(room) {
+    log("Received request to create or join room " + room);
 
     var clientsInRoom = io.sockets.adapter.rooms[room];
-    var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
-    log('Room ' + room + ' now has ' + numClients + ' client(s)');
+    var numClients = clientsInRoom
+      ? Object.keys(clientsInRoom.sockets).length
+      : 0;
+    log("Room " + room + " now has " + numClients + " client(s)");
 
     if (numClients === 0) {
       socket.join(room);
-      log('Client ID ' + socket.id + ' created room ' + room);
-      socket.emit('created', room, socket.id);
+      log("Client ID " + socket.id + " created room " + room);
+      socket.emit("created", room, socket.id);
     } else if (numClients < ROOM_CAPACITY) {
-      log('Client ID ' + socket.id + ' joined room ' + room);
-      io.sockets.in(room).emit('join', room);
+      log("Client ID " + socket.id + " joined room " + room);
+      io.sockets.in(room).emit("join", room);
       socket.join(room);
-      socket.emit('joined', room, socket.id);
-      io.sockets.in(room).emit('ready');
-    } else { // TODO: max two clients
-      socket.emit('full', room);
+      socket.emit("joined", room, socket.id);
+      io.sockets.in(room).emit("ready");
+    } else {
+      // TODO: max two clients
+      socket.emit("full", room);
     }
   });
 
-  socket.on('ipaddr', function() {
+  socket.on("ipaddr", function() {
     var ifaces = os.networkInterfaces();
     for (var dev in ifaces) {
       ifaces[dev].forEach(function(details) {
-        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
-          socket.emit('ipaddr', details.address);
+        if (details.family === "IPv4" && details.address !== "127.0.0.1") {
+          socket.emit("ipaddr", details.address);
         }
       });
     }
--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
 var localStream;
 var idToConn = {};
 
 var iceConfig = {
-  'iceServers': [
-	  {'urls': 'stun:stun.stunprotocol.org:3478'},
-    {'urls': 'stun:stun.l.google.com:19302'},
+  iceServers: [
+    { urls: "stun:stun.stunprotocol.org:3478" },
+    { urls: "stun:stun.l.google.com:19302" }
   ]
 };
 
-var room = 'foo';
+var room = "foo";
 // Could prompt for room name:
 // room = prompt('Enter room name:');
 
@@ -23,88 +23,88 @@ txtInput.addEventListener("keyup", function(event) {
     event.preventDefault();
     sendButton.click();
   }
-})
+});
 
 sendButton.onclick = sendText;
 function sendText() {
   var text = txtInput.value;
   if (text !== "") {
-    addMessage('(Myself)', text);
+    addMessage("(Myself)", text);
     for (var id in idToConn) {
       var conn = idToConn[id];
       conn.textChannel.send(text);
     }
-    txtInput.value = ''
+    txtInput.value = "";
   }
 }
 
 var chatContainer = document.getElementById("chat-container");
 function addMessage(senderName, message) {
   var msg = document.createElement("div");
-  msg.classList.add('message');
-  msg.innerText = senderName + ' : ' + message;
+  msg.classList.add("message");
+  msg.innerText = senderName + " : " + message;
 
   chatContainer.appendChild(msg);
   chatContainer.scrollTop = chatContainer.scrollHeight;
 }
 
-if (room !== '') {
-  socket.emit('create or join', room);
-  console.log('Attempted to create or  join room', room);
+if (room !== "") {
+  socket.emit("create or join", room);
+  console.log("Attempted to create or  join room", room);
 }
 
-socket.on('created', function(room) {
-  console.log('Created room ' + room);
+socket.on("created", function(room) {
+  console.log("Created room " + room);
 });
 
-socket.on('full', function(room) {
-  console.log('Room ' + room + ' is full');
+socket.on("full", function(room) {
+  console.log("Room " + room + " is full");
 });
 
 // Other person join
-socket.on('join', function (room){
-  console.log('Another peer made a request to join room ' + room);
+socket.on("join", function(room) {
+  console.log("Another peer made a request to join room " + room);
 });
 
 // I joined
-socket.on('joined', function(room) {
-  console.log('joined: ' + room);
+socket.on("joined", function(room) {
+  console.log("joined: " + room);
 });
 
-socket.on('log', function(array) {
+socket.on("log", function(array) {
   console.log.apply(console, array);
 });
 
 ////////////////////////////////////////////////
 
 function broadcastMessage(message) {
-  console.log('Client broadcasting message: ', message);
-  socket.emit('broadcast', message);
+  console.log("Client broadcasting message: ", message);
+  socket.emit("broadcast", message);
 }
 
 function sendMessage(target, message) {
-  console.log('Client sending message: ', target, message);
-  socket.emit('message', [target, message]);
+  console.log("Client sending message: ", target, message);
+  socket.emit("message", [target, message]);
 }
 
 // This client receives a message
-socket.on('message', function(packedMessage) {
-  console.log('Client received message:', packedMessage);
-  var[sender, message] = packedMessage;
-  if (message === 'got user media') {
+socket.on("message", function(packedMessage) {
+  console.log("Client received message:", packedMessage);
+  var [sender, message] = packedMessage;
+  if (message === "got user media") {
     var newConn = createConnection(sender);
     doCall(newConn);
-  } else if (message.type === 'offer') {
+  } else if (message.type === "offer") {
     // TODO : check duplicated offer
     var newConn = createConnection(sender);
     newConn.setRemoteDescription(new RTCSessionDescription(message));
     doAnswer(newConn);
-  } else if (message.type === 'answer') {
+  } else if (message.type === "answer") {
     if (sender in idToConn) {
       var conn = idToConn[sender];
       conn.setRemoteDescription(new RTCSessionDescription(message));
     }
-  } else if (message.type === 'candidate') {
+  } else if (message.type === "candidate") {
     if (sender in idToConn) {
       var candidate = new RTCIceCandidate({
         sdpMLineIndex: message.label,
@@ -113,46 +113,53 @@ socket.on('message', function(packedMessage) {
       var conn = idToConn[sender];
       conn.addIceCandidate(candidate);
     }
-  } else if (message === 'bye') {
+  } else if (message === "bye") {
     handleRemoteHangup(sender);
   }
 });
 
 ////////////////////////////////////////////////////
-navigator.mediaDevices.getUserMedia({
-  audio: true
-})
-.then(gotStream)
-.catch(function(e) {
-  alert('getUserMedia() error: ' + e.name);
-});
+navigator.mediaDevices
+  .getUserMedia({
+    audio: true
+  })
+  .then(gotStream)
+  .catch(function(e) {
+    alert("getUserMedia() error: " + e.name);
+  });
 
 function gotStream(stream) {
-  console.log('Adding local stream.');
+  console.log("Adding local stream.");
   localStream = stream;
-  broadcastMessage('got user media');
+  broadcastMessage("got user media");
 }
 
 function createConnection(sender) {
-  var newConn = null
-  console.log('>>>>>>> createConnection() ', localStream);
-  if (typeof localStream !== 'undefined') {
-    console.log('>>>>>> creating peer connection');
+  var newConn = null;
+  console.log(">>>>>>> createConnection() ", localStream);
+  if (typeof localStream !== "undefined") {
+    console.log(">>>>>> creating peer connection");
     newConn = new RTCPeerConnection(iceConfig);
     newConn.userDOM = createUserDOM(sender);
-    newConn.textChannel = newConn.createDataChannel('sendDataChannel', null);
-    newConn.ondatachannel = (event) => {
-      event.channel.onmessage = (event) => {
-      console.log("Data come");
-      addMessage(sender, event.data);
-      }
-    }
+    newConn.textChannel = newConn.createDataChannel("sendDataChannel", null);
+    newConn.ondatachannel = event => {
+      event.channel.onmessage = event => {
+        console.log("Data come");
+        addMessage(sender, event.data);
+      };
+    };
     newConn.senderID = sender;
-    newConn.onicecandidate = (event) => {handleIceCandidate(newConn, event);};
-    newConn.onaddstream = (event) => {handleRemoteStreamAdded(newConn.userDOM, event);};
-    newConn.onremovestream = (event) => {handleRemoteStreamRemoved(newConn.userDOM, event);};
+    newConn.onicecandidate = event => {
+      handleIceCandidate(newConn, event);
+    };
+    newConn.onaddstream = event => {
+      handleRemoteStreamAdded(newConn.userDOM, event);
+    };
+    newConn.onremovestream = event => {
+      handleRemoteStreamRemoved(newConn.userDOM, event);
+    };
     newConn.addStream(localStream);
-    console.log('Created RTCPeerConnnection');
+    console.log("Created RTCPeerConnnection");
 
     idToConn[sender] = newConn;
   }
@@ -165,58 +172,59 @@ window.onbeforeunload = function() {
 };
 
 function handleIceCandidate(conn, event) {
-  console.log('icecandidate event: ', event);
+  console.log("icecandidate event: ", event);
   if (event.candidate) {
     sendMessage(conn.senderID, {
-      type: 'candidate',
+      type: "candidate",
       label: event.candidate.sdpMLineIndex,
       id: event.candidate.sdpMid,
       candidate: event.candidate.candidate
     });
   } else {
-    console.log('End of candidates.');
+    console.log("End of candidates.");
   }
 }
 
 function handleCreateOfferError(event) {
-  console.log('createOffer() error: ', event);
+  console.log("createOffer() error: ", event);
 }
 
 function doCall(conn) {
-  console.log('Sending offer to peer');
-  conn.createOffer( (sessionDescription) => {setLocalAndSendMessage(conn, sessionDescription);}, handleCreateOfferError);
+  console.log("Sending offer to peer");
+  conn.createOffer(sessionDescription => {
+    setLocalAndSendMessage(conn, sessionDescription);
+  }, handleCreateOfferError);
 }
 
 function doAnswer(conn) {
-  console.log('Sending answer to peer.');
-  conn.createAnswer().then(
-    (sessionDescription) => {setLocalAndSendMessage(conn, sessionDescription);},
-    onCreateSessionDescriptionError
-  );
+  console.log("Sending answer to peer.");
+  conn.createAnswer().then(sessionDescription => {
+    setLocalAndSendMessage(conn, sessionDescription);
+  }, onCreateSessionDescriptionError);
 }
 
 function setLocalAndSendMessage(conn, sessionDescription) {
   conn.setLocalDescription(sessionDescription);
-  console.log('setLocalAndSendMessage sending message', sessionDescription);
+  console.log("setLocalAndSendMessage sending message", sessionDescription);
   sendMessage(conn.senderID, sessionDescription);
 }
 
 function onCreateSessionDescriptionError(error) {
-  trace('Failed to create session description: ' + error.toString());
+  trace("Failed to create session description: " + error.toString());
 }
 
 function handleRemoteStreamAdded(dom, event) {
-  console.log('Remote stream added.');
+  console.log("Remote stream added.");
   dom.lastElementChild.srcObject = event.stream;
 }
 
 function handleRemoteStreamRemoved(dom, event) {
-  console.log('Remote stream removed. Event: ', event);
-  document.getElementById('videos').removeChild(dom);
+  console.log("Remote stream removed. Event: ", event);
+  document.getElementById("videos").removeChild(dom);
 }
 
 function hangup() {
-  console.log('Hanging up.');
+  console.log("Hanging up.");
   for (var id in idToConn) {
     var conn = idToConn[id];
     conn.close();
@@ -225,17 +233,17 @@ function hangup() {
 
 function handleRemoteHangup(id) {
   if (id in idToConn) {
-    console.log('Other said bye.');
+    console.log("Other said bye.");
     var conn = idToConn[id];
     conn.close();
-    document.getElementById('videos').removeChild(conn.userDOM);
+    document.getElementById("videos").removeChild(conn.userDOM);
     delete idToConn[id];
   }
 }
 
 function createUserDOM(id) {
   var node = document.createElement("div");
-  node.classList.add('row')
+  node.classList.add("row");
   node.id = id;
 
   var label = document.createElement("label");
@@ -247,18 +255,18 @@ function createUserDOM(id) {
   audio.playsinline = true;
 
   var slider = document.createElement("input");
-  slider.id = 'slider' + id;
-  slider.type = 'range';
+  slider.id = "slider" + id;
+  slider.type = "range";
   slider.min = 0;
   slider.max = 100;
   slider.value = 75;
   slider.oninput = function() {
     audio.volume = slider.value / 100;
-  }
+  };
   node.appendChild(label);
   node.appendChild(slider);
   node.appendChild(audio);
 
-  document.getElementById('videos').appendChild(node);
+  document.getElementById("videos").appendChild(node);
   return node;
 }
